d2957836f8d59b800c799780b839c4bd3427f6cc
nchok <nchok462@ceermotors.com>
2025-12-14 09:26:46 +0300

    Add Git configuration script for repository setup

---
+++ git_config.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Configure Git for this repository
+"C:\PortableGit\bin\git.exe" config user.email "nchok462@ceermotors.com"
+"C:\PortableGit\bin\git.exe" config user.name "nchok"
+echo Git configured successfully!
+echo Email: nchok462@ceermotors.com
+echo Name: nchok
+pause
